57a849fb54ef9ac1bb1e1fbf20ff673376265099
Gabriel Jose da Silva dos Santos <gabriel.ssantos@totvs.com.br>
2022-08-20 15:22:34 -0300

    Added diamond image to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'I Am Rich',
-    home: Scaffold(backgroundColor: Colors.blueGrey),
+    home: Home(),
   ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Center(child: Text('I Am Rich')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    );
+  }
 }
