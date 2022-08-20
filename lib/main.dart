704c32142e5299c425e173cf9f778a89eb3f4a09
Gabriel Jose da Silva dos Santos <gabriel.ssantos@totvs.com.br>
2022-08-20 15:05:24 -0300

    Added project files

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'I Am Rich',
+    home: Scaffold(backgroundColor: Colors.blueGrey),
+  ));
+}
